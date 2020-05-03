2b7141c0e94cca70b7cc8d7f252c866151f54a0f
Marcos Corsi <marcosvcorsi@gmail.com>
2020-05-03 00:27:07 -0300

    Finalizando CRUD de usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,4 +6,33 @@ module.exports = {
 
     return response.json(users);
   },
+
+  async store(request, response) {
+    const { username } = request.body;
+
+    await knex('users').insert({ username });
+
+    return response.status(201).send();
+  },
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { username } = request.body;
+
+    await knex('users').update({ username }).where({
+      id,
+    });
+
+    return response.send();
+  },
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex('users').delete().where({
+      id,
+    });
+
+    return response.status(204).send();
+  },
 };
--- src/server.js
+++ src/server.js
@@ -3,6 +3,18 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
+
+routes.use((request, response, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+routes.use((error, request, response, next) => {
+  response.status(error.status || 500);
+  response.json({ error: error.message });
+});
 
 app.listen(3333, () => console.log('Server is running'));
